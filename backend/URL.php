4e3e659a58f6cbbc9c4d781874ff270b63197737
Ryan Statzer <devnull@localhost>
2011-01-28 20:24:38 -0500

    Fixing URL.php to work in a root situation like "http://skylight.rdro.us" instead of like "http://skylight.rdro.us/blog"

--- backend/URL.php
+++ backend/URL.php
@@ -1,9 +1,9 @@
 <?php
 class URL
-{	
+{
 	private static $requestHandled;
 	private static $knownURLs = array();
-	
+
 	public static function setSystemURLs()
 	{
 		$knownURLs = array_push(self::$knownURLs,array("name" => "404", "matchto" => "Nothing", "handler" => "ThemeHandler", "action" => "display_404"));
@@ -15,48 +15,54 @@ class URL
 	}
 	public static function base()
 	{
-		$script_path = explode("/", $_SERVER['REQUEST_URI']);		
-		return "http://" . $_SERVER['SERVER_NAME'] . "/" . $script_path[1] . "/";
+		$script_path = explode("/", $_SERVER['REQUEST_URI']);
+        	if ($script_path[1] == ""){return "http://" . $_SERVER['SERVER_NAME'];}
+	        else {return "http://" . $_SERVER['SERVER_NAME'] . "/" . $script_path[1] . "/";}
 	}
 	public static function scriptPath()
 	{
 		$script_path = explode("/", $_SERVER['REQUEST_URI']);
-		return "/" . $script_path[1];
+//		echo $_SERVER['REQUEST_URI'];
+//		print_r($script_path);
+		if ($script_path[1] == ""){return "/";}
+		else {return "/" . $script_path[1];}
 	}
 	public static function parse($url)
 	{
-		self::setSystemURLs();		
+		self::setSystemURLs();
 		$parsedURL = str_replace(self::scriptPath(), "", $url);
+		if ($parsedURL == "") {$parsedURL == self::scriptPath();}
+		//else {$parsedURL = self::scriptPath();}
 		$actionArgs = explode("/", $parsedURL);
-		if ($actionArgs[1] == "login") 
+		echo $parsedURL;
+		if (count($actionArgs) > 1 && $actionArgs[1] == "login")
 		{
 			self::activateHandler("ThemeHandler", "displayLogin", "");
-			self::$requestHandled = true; 			
+			self::$requestHandled = true;
 		}
-		if ($parsedURL == "/") 
+		if ($parsedURL == "/" || $parsedURL = "")
 		{
-			self::activateHandler("ThemeHandler", "home", "");			
-			self::$requestHandled = true;			
+			self::activateHandler("ThemeHandler", "home", "");
+			self::$requestHandled = true;
 		}
 		foreach (self::$knownURLs as $handler => $hand)
-		{			
-			if ($hand['name'] != "404" && $hand['name'] != "home" && self::$requestHandled == false)			
-			{				
-				if (preg_match($hand['matchto'], $parsedURL)) 
-				{					
-					self::activateHandler($hand['handler'], $hand['action'], $actionArgs);					
-					self::$requestHandled = true;					
-				}			
+		{
+			if ($hand['name'] != "404" && $hand['name'] != "home" && self::$requestHandled == false)
+			{
+				if (preg_match($hand['matchto'], $parsedURL))
+				{
+					self::activateHandler($hand['handler'], $hand['action'], $actionArgs);
+					self::$requestHandled = true;
+				}
 			}
 		}
-		if (self::$requestHandled == false) 
+		if (self::$requestHandled == false)
 		{
-			$handler = self::getHandler("404");			
+			$handler = self::getHandler("404");
 			$han = new $handler();
-			$han->action = "404";
 			$han->args = "";
-			$han->act();
-		}		
+			$han->act("404");
+		}
 		else {return true;}
 	}
 	public static function addToURLList($url)
@@ -64,7 +70,7 @@ class URL
 		self::$knownURLs[] = $url;
 	}
 	private static function getHandler($name)
-	{		
+	{
 		foreach (self::$knownURLs as $handler => $hand)
 		{
 			if ($hand['name'] == $name) 
@@ -90,7 +96,7 @@ class URL
 		$hand->name = $name;
 		$hand->action = $action;
 		$hand->args = $args;
-		$hand->act($name);		
+		$hand->act($name);
 	}*/
 }
 ?>
--- backend/config.php
+++ backend/config.php
@@ -1,8 +1,8 @@
 <?php
 $config['db-server'] = "localhost";
-$config['db-name'] = "reddroid_clients";
+$config['db-name'] = "skylight";
 $config['db-user'] = "root";
-$config['db-pass'] = "";
+$config['db-pass'] = "rqnd2759_";
 //TODO: Put stuff below here in a database table instead of here.
 $config['site-name'] = "blackheart";
 $config['style'] = "H2";
@@ -12,5 +12,5 @@ $config['debug'] = false;
 /* CHANGE NOTHING BELOW THIS LINE */
 $config['version'] = "0.3-dev";
 $config['package-content-withL5A'] = true;
-$config['base-path'] = "/news/";
+$config['base-path'] = "/";
 ?>
